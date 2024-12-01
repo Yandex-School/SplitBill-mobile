d54839f537e0a5bc37de75a6f8b89ed3ce6b0544
Jasmina06 <abduhakimovayasmina14@gmail.com>
2024-12-01 19:15:13 +0500

    added provider scope

---
+++ lib/core/scope/provider_scope.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:split_bill/presentation/event_room/data/repositories/mock_repository.dart';
+import 'package:split_bill/presentation/event_room/domain/controllers/list_controller.dart';
+import 'package:split_bill/presentation/login_page/domain/providers/login_provider.dart';
+
+class ProviderScope extends StatelessWidget {
+  final Widget child;
+  const ProviderScope({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ListController(MockRepository()),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+        ),
+      ],
+      child: child,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:split_bill/presentation/event_room/data/repositories/mock_repository.dart';
-import 'package:split_bill/presentation/event_room/domain/controllers/list_controller.dart';
+import 'package:split_bill/core/scope/provider_scope.dart';
 import 'package:split_bill/presentation/event_room/presentation/pages/list_page.dart';
-import 'package:split_bill/presentation/login_page/domain/providers/login_provider.dart';
 import 'package:split_bill/presentation/login_page/pages/login_page.dart';
 import 'package:split_bill/presentation/login_page/pages/signup_page.dart';
 import 'package:split_bill/presentation/logo_page/animation.dart';
 
-
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,28 +16,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => ListController(MockRepository()),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => LoginProvider(), 
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'SplitBill',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const ConcentricAnimationOnboarding(),
-          '/list': (context) => const ListPage(),
-          '/login': (context) => const LoginPage(),
-          '/signup': (context) => const SignUpPage(),
-        },
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SplitBill',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const ConcentricAnimationOnboarding(),
+        '/list': (context) => const ListPage(),
+        '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignUpPage(),
+      },
     );
   }
 }
-
