57641cdcb8a0306d86fbb240016730ace42a476a
Jasmina06 <abduhakimovayasmina14@gmail.com>
2024-12-06 13:26:10 +0500

    switched LoginProvider to DI

--- lib/features/login/presentation/screen/login_screen.dart
+++ lib/features/login/presentation/screen/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 import 'package:split_bill/core/utils/text_utils.dart';
 import 'package:split_bill/core/widgets/custom_button.dart';
 import 'package:split_bill/features/login/presentation/provider/login_provider.dart';
@@ -10,132 +10,126 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<LoginProvider>(
-      create: (_) => LoginProvider(),
-      child: Consumer<LoginProvider>(
-        builder: (context, loginProvider, child) => Scaffold(
-          backgroundColor: const Color(0xff051326),
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: const Color(0xff051326),
-            elevation: 0.0,
-          ),
-          body: SafeArea(
-            child: Center(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Вход",
-                        style: TextUtils.headingStyle,
-                      ),
-                      const SizedBox(height: 10),
+    final loginProvider = GetIt.instance<LoginProvider>();
 
-                      // Подзаголовок
-                      const Text(
-                        "Пожалуйста войдите чтобы продолжить работу",
-                        style: TextUtils.subtitleStyle,
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 40),
-                      TextField(
-                        onChanged: loginProvider.setEmail,
-                        decoration: InputDecoration(
-                          labelText: "Имя",
-                          labelStyle: const TextStyle(
-                            color: TextUtils.grey,
-                            fontSize: 14,
-                          ),
-                          prefixIcon: const Icon(Icons.email, color: TextUtils.grey),
-                          errorText: loginProvider.isValidEmail
-                              ? null
-                              : "Введите действительный адрес электронной почты",
-                          filled: true,
-                          fillColor: const Color(0xff051326),
-                          border: const OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-
-                      // Поле ввода пароля
-                      TextField(
-                        onChanged: loginProvider.setPassword,
-                        obscureText: loginProvider.showPassword,
-                        decoration: InputDecoration(
-                          labelText: "Пароль",
-                          labelStyle: const TextStyle(
-                            color: TextUtils.grey,
-                            fontSize: 14,
-                          ),
-                          prefixIcon: const Icon(Icons.lock, color: TextUtils.grey),
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              loginProvider.showPassword
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
-                              color: TextUtils.grey,
-                            ),
-                            onPressed: loginProvider.toggleShowPassword,
-                          ),
-                          errorText: loginProvider.isValidPassword ? null : "Неверный пароль",
-                          filled: true,
-                          fillColor: const Color(0xff051326),
-                          border: const OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 40),
-
-                      // Кнопка входа
-                      CustomButton(
-                        text: "Вход",
-                        onPressed: () {
-                          if (loginProvider.validateCredentials()) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Вход в систему прошел успешно!')),
-                            );
-                            context.go('/event-rooms');
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Неверные учетные данные!')),
-                            );
-                          }
-                        },
-                      ),
-                      const SizedBox(height: 40),
-
-                      // Регистрация
-                      Center(
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      const Text(
-        "Нет аккаунта?",
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 14,
-        ),
+    return Scaffold(
+      backgroundColor: const Color(0xff051326),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xff051326),
+        elevation: 0.0,
       ),
-      GestureDetector(
-        onTap: () => context.go('/login/sign-up'),
-        child: const Text(
-          " Регистрация",
-          style: TextStyle(
-            fontSize: 14,
-            color: Colors.orange,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    ],
-  ),
-),
-
-                    ],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Вход",
+                    style: TextUtils.headingStyle,
                   ),
-                ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "Пожалуйста войдите чтобы продолжить работу",
+                    style: TextUtils.subtitleStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 40),
+                  TextField(
+                    onChanged: loginProvider.setEmail,
+                    decoration: InputDecoration(
+                      labelText: "Имя",
+                      labelStyle: const TextStyle(
+                        color: TextUtils.grey,
+                        fontSize: 14,
+                      ),
+                      prefixIcon: const Icon(Icons.email, color: TextUtils.grey),
+                      errorText: loginProvider.isValidEmail
+                          ? null
+                          : "Введите действительный адрес электронной почты",
+                      filled: true,
+                      fillColor: const Color(0xff051326),
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  // Поле ввода пароля
+                  TextField(
+                    onChanged: loginProvider.setPassword,
+                    obscureText: loginProvider.showPassword,
+                    decoration: InputDecoration(
+                      labelText: "Пароль",
+                      labelStyle: const TextStyle(
+                        color: TextUtils.grey,
+                        fontSize: 14,
+                      ),
+                      prefixIcon: const Icon(Icons.lock, color: TextUtils.grey),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          loginProvider.showPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: TextUtils.grey,
+                        ),
+                        onPressed: loginProvider.toggleShowPassword,
+                      ),
+                      errorText: loginProvider.isValidPassword ? null : "Неверный пароль",
+                      filled: true,
+                      fillColor: const Color(0xff051326),
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+
+                  // Кнопка входа
+                  CustomButton(
+                    text: "Вход",
+                    onPressed: () {
+                      if (loginProvider.validateCredentials()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Вход в систему прошел успешно!')),
+                        );
+                        context.go('/event-rooms');
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Неверные учетные данные!')),
+                        );
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 40),
+
+                  // Регистрация
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Нет аккаунта?",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => context.go('/login/sign-up'),
+                          child: const Text(
+                            " Регистрация",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.orange,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,20 @@ import 'package:split_bill/core/DI/dependencies_config.dart';
 import 'package:split_bill/core/router/app_router.dart';
 import 'package:split_bill/core/scope/provider_scope.dart';
 import 'package:split_bill/core/theme/theme_notifier.dart';
+import 'package:get_it/get_it.dart';
+import 'package:split_bill/features/login/presentation/provider/login_provider.dart';
 
 Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupDependencies();
+
   runApp(
     ProviderScope(
-      child: ChangeNotifierProvider(
-        create: (_) => ThemeNotifier(),
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => ThemeNotifier()),
+          ChangeNotifierProvider(create: (_) => GetIt.instance<LoginProvider>()),
+        ],
         child: SplitBills(),
       ),
     ),
